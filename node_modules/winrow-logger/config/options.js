9d5683ec5b8a177c7469938e5f4ef8e9135d28f2
NguyenPhuocMinh <xiuquay55@gmail.com>
2020-11-10 21:35:53 +0700

    update winrow and winrow-logger

--- node_modules/winrow-logger/config/options.js
+++ node_modules/winrow-logger/config/options.js
@@ -7,17 +7,13 @@ const config = {
     warn: 2,
     data: 3,
     info: 4,
-    verbose: 5,
-    silly: 6,
   },
   colors: {
-    error: 'red',
-    debug: 'blue',
-    warn: 'yellow',
-    data: 'grey',
-    info: 'green',
-    verbose: 'cyan',
-    silly: 'pink',
+    error: 'bold red cyanBG',
+    debug: 'italic blue',
+    warn: 'italic yellow',
+    data: 'magenta',
+    info: 'bold green',
   }
 };
 
--- node_modules/winrow-logger/lib/logger.js
+++ node_modules/winrow-logger/lib/logger.js
@@ -8,13 +8,14 @@ winston.addColors(options.colors);
 const Logger = winston.createLogger({
   levels: options.levels,
   format: winston.format.combine(
+    winston.format.json(),
     winston.format.colorize({ all: true }),
     winston.format.simple(),
     winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
     winston.format.label({ label: '[LOGGER]' }),
     winston.format.printf(info => {
       return `${info.label}-${info.level}-[${info.timestamp}]-[${info.requestId}]: ${info.message}`;
-    })
+    }),
   ),
   transports: [
     new winston.transports.Console()
