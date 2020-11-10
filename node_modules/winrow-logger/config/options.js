e50230106ffe5fb90233e7217c364de1ea2ca88f
NguyenPhuocMinh <xiuquay55@gmail.com>
2020-11-10 09:15:18 +0700

    add logger for dataStore

---
+++ node_modules/winrow-logger/config/options.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const config = {
+  levels: {
+    error: 0,
+    debug: 1,
+    warn: 2,
+    data: 3,
+    info: 4,
+    verbose: 5,
+    silly: 6,
+  },
+  colors: {
+    error: 'red',
+    debug: 'blue',
+    warn: 'yellow',
+    data: 'grey',
+    info: 'green',
+    verbose: 'cyan',
+    silly: 'pink',
+  }
+};
+
+module.exports = config;
