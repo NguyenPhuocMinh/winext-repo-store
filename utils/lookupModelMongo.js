d3a9a35ccb6cfcccbb39b255983cf38d92860906
Minh Nguyen <xiuquay55@gmail.com>
2021-06-22 09:53:29 +0700

    add lookup model sequelize

--- utils/lookupModelMongo.js
+++ utils/lookupModelMongo.js
@@ -4,10 +4,11 @@ const winext = require('winext');
 const lodash = winext.require('lodash');
 const mongoose = winext.require('mongoose');
 const { models } = mongoose;
+const { includes } = lodash;
 
 function LookupModelMongo(schemaModels, type) {
   let model = null;
-  if (lodash.includes(schemaModels, models[type])) {
+  if (includes(schemaModels, models[type])) {
     return model = models[type];
   }
   throw new Error('InvalidNameModel');
--- utils/lookupModelSql.js
+++ utils/lookupModelSql.js
@@ -1,11 +1,17 @@
 'use strict';
 
 const winext = require('winext');
+const lodash = winext.require('lodash');
 const { sequelize } = require('../lib/sequelizeStore');
-console.log("🚀 ~ file: lookupModelSql.js ~ line 5 ~ sequelize", sequelize.models)
+const { models } = sequelize;
+const { includes } = lodash;
 
 function LookupModelSql(schemaModels, type) {
-
+  let model = null;
+  if (includes(schemaModels), models[type]) {
+    return model = models[type];
+  }
+  throw new Error('InvalidNameModel');
 };
 
 module.exports = LookupModelSql;
