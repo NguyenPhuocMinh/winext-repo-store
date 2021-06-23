4d42b0c53d332351adb190059bb0bd58c0fab7af
Minh Nguyen <xiuquay55@gmail.com>
2021-06-23 16:35:45 +0700

    fix bug for models is not define

--- utils/lookupModelSql.js
+++ utils/lookupModelSql.js
@@ -2,9 +2,11 @@
 
 const winext = require('winext');
 const lodash = winext.require('lodash');
-const { includes } = lodash;
+const { get, includes } = lodash;
 
-function LookupModelSql(schemaModels, type) {
+function LookupModelSql(schemaModels, type, sequelize) {
+  const models = get(sequelize, 'models');
+  
   let model = null;
   if (includes(schemaModels, models[type])) {
     return model = models[type];
