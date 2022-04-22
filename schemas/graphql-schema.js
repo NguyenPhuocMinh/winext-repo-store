d44c955626828f4e278979d6b5dfc37e98c02266
Minh Nguyen <xiuquay55@gmail.com>
2022-04-23 00:18:44 +0700

    update future

--- schemas/graphql-schema.js
+++ schemas/graphql-schema.js
@@ -4,14 +4,15 @@ const winext = require('winext');
 const { GraphQLSchema } = winext.require('graphql');
 
 const convertGraphqlSchema = (modelDescriptor = []) => {
-  const schema = new GraphQLSchema({
-    query: {},
-    // If you need to create or updata a datasource,
-    // you use mutations. Note:
-    // mutations will not be explored in this post.
-    // mutation: BlogMutationRootType
-  });
-  return schema;
+  // const schema = new GraphQLSchema({
+  //   query: {},
+  //   // If you need to create or updata a datasource,
+  //   // you use mutations. Note:
+  //   // mutations will not be explored in this post.
+  //   // mutation: BlogMutationRootType
+  // });
+  // return schema;
+  return {};
 };
 
 module.exports = convertGraphqlSchema;
