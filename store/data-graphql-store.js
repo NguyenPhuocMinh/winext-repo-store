66ddb8671336d8cb237320583ce9bebe8b393f7b
Minh Nguyen <xiuquay55@gmail.com>
2022-04-28 00:15:58 +0700

    update future

--- store/data-graphql-store.js
+++ store/data-graphql-store.js
@@ -1,11 +1,25 @@
 'use strict';
 
 const winext = require('winext');
+const Promise = winext.require('bluebird');
+const lodash = winext.require('lodash');
+const { graphql } = winext.require('graphql');
+const { get } = lodash;
 
 function DataGraphqlStore(params = {}) {
-  this.data = function () {
-    return 'data';
+  const schema = get(params, 'modelGraphqlDescriptor', {});
+
+  this.queryData = async function ({ type }) {
+    try {
+      const data = await graphql({ schema: schema, source: type });
+      console.log('🚀 ~ file: data-graphql-store.js ~ line 15 ~ data', data);
+      return 'data';
+    } catch (err) {
+      return Promise.reject(err);
+    }
   };
+
+  this.mutationData = async function ({ operationName, variables = {} }) {};
 }
 
 exports = module.exports = new DataGraphqlStore();
