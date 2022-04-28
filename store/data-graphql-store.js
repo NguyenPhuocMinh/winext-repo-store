513040c54060b1aae2c99acd18a2a237cf76a230
Minh Nguyen <xiuquay55@gmail.com>
2022-04-28 15:08:00 +0700

    update future

--- store/data-graphql-store.js
+++ store/data-graphql-store.js
@@ -4,32 +4,96 @@ const winext = require('winext');
 const Promise = winext.require('bluebird');
 const lodash = winext.require('lodash');
 const fetch = winext.require('node-fetch');
-const { get } = lodash;
+const dotenv = winext.require('dotenv');
+const { get, isEmpty } = lodash;
+const { mutation } = require('gql-query-builder');
 
 function DataGraphqlStore(params = {}) {
-  const modelDescriptor = get(params, 'modelDescriptor', {});
+  const requestId = get(params, 'requestId');
+  const loggerFactory = get(params, 'loggerFactory');
+  const graphqlConfig = get(params, 'config.graphql');
+  // config env
+  dotenv.config();
+  const protocol = process.env.PROTOCOL || 'http';
+  const portServer = process.env.SERVER_PORT;
+  const hostServer = process.env.SERVER_HOST;
+  const pathGraphql = process.env.GRAPHQL_PATH || get(graphqlConfig, 'path', '/graphql');
 
-  this.queryData = async function ({ operationName, fields = [] }) {
+  const endpoint = `${protocol}://${hostServer}:${portServer}${pathGraphql}`;
+
+  this.queryData = async function ({ operationName, returnFields = [], variables = {} }) {
     try {
+      loggerFactory.warn(`queryData has been start with`, {
+        requestId: `${requestId}`,
+        args: {
+          operationName: operationName,
+          fields: returnFields,
+        },
+      });
       const query = `
         query RootQuery {
          ${operationName} {
-            ${fields}
+            ${returnFields}
           }
         }`;
-      const response = fetch('http://localhost:7979/graphql', {
+      const response = await fetch(endpoint, {
         method: 'POST',
-        body: JSON.stringify({ query, variables: {} }),
+        body: JSON.stringify({ query, variables }),
+        headers: { 'Content-Type': 'application/json' },
       });
-      const data = await response.text();
-      console.log("🚀 ~ file: data-graphql-store.js ~ line 25 ~ data", data)
-      return 'data';
+      const data = await response.json();
+      loggerFactory.warn(`queryData has been end`, {
+        requestId: `${requestId}`,
+      });
+      return !isEmpty(data.data) ? data.data : {};
     } catch (err) {
+      loggerFactory.error(`queryData graphql has been error: ${err}`, {
+        requestId: `${requestId}`,
+      });
       return Promise.reject(err);
     }
   };
 
-  this.mutationData = async function ({ operationName, variables = {} }) {};
+  this.mutationData = async function ({ operationName, returnFields = [], dataFields, variables = {} }) {
+    try {
+      loggerFactory.warn(`mutationData has been start with`, {
+        requestId: `${requestId}`,
+        args: {
+          operationName: operationName,
+          variables: variables,
+        },
+      });
+      const query = mutation({
+        operation: 'thoughtCreate',
+        variables: {
+          name: 'Tyrion Lannister',
+          thought: 'I drink and I know things.',
+        },
+        fields: ['id'],
+      });
+      // const mutation = `
+      //   mutation RootMutation {
+      //    ${operationName}(${dataFields}) {
+      //       ${returnFields}
+      //     }
+      //   }`;
+      const response = await fetch(endpoint, {
+        method: 'POST',
+        body: JSON.stringify({ query, variables: variables }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+      const data = await response.json();
+      loggerFactory.warn(`mutationData has been end`, {
+        requestId: `${requestId}`,
+      });
+      return !isEmpty(data.data) ? data.data : {};
+    } catch (err) {
+      loggerFactory.error(`mutationData graphql has been error: ${err}`, {
+        requestId: `${requestId}`,
+      });
+      return Promise.reject(err);
+    }
+  };
 }
 
 exports = module.exports = new DataGraphqlStore();
